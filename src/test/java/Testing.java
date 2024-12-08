9c0d22c4b21d15dca974120d5857937134dad932
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-12-07 21:51:09 -0500

    Test: Created test function for returning the current date and time

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -1,12 +1,17 @@
 import static org.junit.jupiter.api.Assertions.*;
+import java.time.*;
 
 import org.junit.jupiter.api.Test;
 
 class Testing {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    void testGetCurrentDateTime() {
+        
+		LocalDateTime now = DateTimeFunctions.getCurrentDateTime();
+       
+        assertNotNull(now, "Current date and time should not be null");
+   
+	}//end of testGetCurrentDateTime method 
 
-}
+}//end of Testing class 
