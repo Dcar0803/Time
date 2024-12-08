09233921d2baa4817d45f3f8802b6d49bb006c3c
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-12-07 22:12:20 -0500

    Doc: Created documentation for the convertDataToISO80561() function

--- src/main/java/DateTimeFunctions.java
+++ src/main/java/DateTimeFunctions.java
@@ -16,6 +16,14 @@ public class DateTimeFunctions {
 	}//end of getCurrentDateTime
 	
 	
+	/**
+     * Converts a given date and time to an ISO 8061 UTC time string.
+     *
+     * @param dateTime The date and time to be converted
+     * @return A string in ISO 8061 UTC format (e.g., 2024-12-03T14:10:31Z)
+     */
+	
+	
 	 public static String convertDateToISO8061(LocalDateTime dateTime) {
 	        
 		 ZonedDateTime utcDateTime = dateTime.atZone(ZoneId.systemDefault()).withZoneSameInstant(ZoneOffset.UTC);
