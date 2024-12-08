40873479a2a26f2575b38b64ea9a462b8126d829
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-12-07 22:01:59 -0500

    Test: Created test function for converting a date to an ISO 8061 UTC time string

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -13,5 +13,17 @@ class Testing {
         assertNotNull(now, "Current date and time should not be null");
    
 	}//end of testGetCurrentDateTime method 
+	
+	
+	 @Test
+	    void testConvertDateToISO8061() {
+	        
+		 LocalDateTime date = LocalDateTime.of(2024, 12, 3, 14, 10, 31);
+	        
+		 String isoDate = DateTimeFunctions.convertDateToISO8061(date);
+	        
+		 assertEquals("2024-12-03T14:10:31Z", isoDate, "Date should be formatted as ISO 8061 UTC string");
+	    
+	 }//end of testConvertDateToISO8061
 
 }//end of Testing class 
